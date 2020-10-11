4bfa20e3ca310b68d5ba8012f1363d54adac6719
gitmatheo <mateusz.dominiak@stxnext.pl>
2020-10-11 22:43:53 +0200

    change vue config and api url

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,13 +1,5 @@
 const API = {
-  url: 'http://85.89.178.154:8080'
+  url: 'https://app.zozbazylia.pl/api'
 }
-
-// const API = {
-//   url: 'http://localhost:8080'
-// }
-
-// const API = {
-//   url: 'http://localhost:3000'
-// }
 
 export default API
